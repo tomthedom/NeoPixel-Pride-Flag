95012e6523c3d0906b0b07531bedc1e0d53801d1
tomthedom <128471458+tomthedom@users.noreply.github.com>
2023-03-25 23:46:23 -0700

    Update PrideFlag_code.py

--- PrideFlag_code.py
+++ PrideFlag_code.py
@@ -2,16 +2,26 @@
 # Author: Tom the Dom
 # License: MIT
 
-import time
-from time import sleep
 import board
 import neopixel
+from time import sleep
 
 # Data pin on your CircuitPy board
 pixel_pin = board.A1
 
-# The number of NeoPixels
-num_pixels = 240 
+# The number of NeoPixels (preferably a multiple of 6)
+num_pixels = 240
+
+# Width of flag (num_pixels divided by 6, rounded down)
+WIDTH = num_pixels // 6
+
+# Define positions of each flag segment break
+W1 = WIDTH
+W2 = WIDTH * 2
+W3 = WIDTH * 3
+W4 = WIDTH * 4
+W5 = WIDTH * 5
+W6 = WIDTH * 6
 
 # Time in seconds to show each flag
 SHOWTIME = 60
@@ -23,23 +33,45 @@ SLEEPTIME = 1
 ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.01, auto_write=False, pixel_order=ORDER
+    pixel_pin, num_pixels, brightness=0.02, auto_write=False, pixel_order=ORDER
 )
 
 while True:
+    # Transgender Pride flag
+    for t in range(0, num_pixels):
+        if t in range(0, W1):
+            pixels[t] = (65, 175, 222) # Maya Blue
+        elif t in range(W1, W2):
+            pixels[t] = (247, 134, 120) # Amaranth Pink
+        elif t in range(W2, W4):
+            pixels[t] = (255, 255, 255) # White
+        elif t in range(W4, W5):
+            pixels[t] = (247, 134, 120) # Amaranth Pink
+        elif t in range(W5, W6):
+            pixels[t] = (65, 175, 222) # Maya Blue
+        else:
+            pixels[t] = (0,0,0)
+    pixels.show()
+    sleep (SHOWTIME)
+
+    # Blank out all pixels
+    pixels.fill((0,0,0))
+    pixels.show()
+    sleep (SLEEPTIME)
+
     # Classic Pride flag
     for c in range(0, num_pixels):
-        if c in range(0, 40):
+        if c in range(0, W1):
             pixels[c] = (118, 0, 137) # Patriarch Purple
-        elif c in range(40, 80):
+        elif c in range(W1, W2):
             pixels[c] = (0, 68, 255) # Blue
-        elif c in range(80, 120):
+        elif c in range(W2, W3):
             pixels[c] = (0, 129, 31) # La Salle Green
-        elif c in range(120, 160):
+        elif c in range(W3, W4):
             pixels[c] = (255, 239, 0) # Canary Yellow
-        elif c in range(160, 200):
+        elif c in range(W4, W5):
             pixels[c] = (224, 109, 7) # Orange
-        elif c in range(200, 240):
+        elif c in range(W5, W6):
             pixels[c] = (231, 0, 0) # Electric Red
         else:
             pixels[c] = (0,0,0)
@@ -51,38 +83,16 @@ while True:
     pixels.show()
     sleep (SLEEPTIME)
 
-    # Transgender Pride flag
-    for t in range(0, num_pixels):
-        if t in range(0, 40):
-            pixels[t] = (65, 175, 222) # Maya Blue
-        elif t in range(40, 80):
-            pixels[t] = (217, 148, 144) # Amaranth Pink
-        elif t in range(80, 160):
-            pixels[t] = (255, 255, 255) # White
-        elif t in range(160, 200):
-            pixels[t] = (217, 148, 144) # Amaranth Pink
-        elif t in range(200, 240):
-            pixels[t] = (65, 175, 222) # Maya Blue
-        else:
-            pixels[t] = (0,0,0)
-    pixels.show()
-    sleep (SHOWTIME)
-
-    # Blank out all pixels
-    pixels.fill((0,0,0))
-    pixels.show()
-    sleep (SLEEPTIME)
-
     # Pansexual Pride flag
     for p in range(0, num_pixels):
-        if p in range(0, 80):
+        if p in range(0, W2):
             pixels[p] = (5, 174, 255) # Blue Bolt
-        elif p in range(80, 160):
+        elif p in range(W2, W4):
             pixels[p] = (255, 218, 0) # Sizzling Sunrise
-        elif p in range(160, 240):
+        elif p in range(W4, W6):
             pixels[p] = (255, 20, 140) # Deep Pink
         else:
-            pixels[t] = (0,0,0)
+            pixels[p] = (0,0,0)
     pixels.show()
     sleep (SHOWTIME)
 
